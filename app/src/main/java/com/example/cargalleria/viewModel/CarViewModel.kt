0a8513d4d35ebea3420841bb37f674cfb3b9f172
Emre Sari <emresari633@gmail.com>
2024-05-22 17:00:48 +0300

    carViewModel created

---
+++ app/src/main/java/com/example/cargalleria/viewModel/CarViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.cargalleria.viewModel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.cargalleria.data.Car
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.toObject
+
+class CarViewModel : ViewModel() {
+    private val _cars = MutableLiveData<List<Car>>(emptyList())
+    private val db = Firebase.firestore
+    private val carsCollection = db.collection("cars")
+
+    val cars: LiveData<List<Car>> = _cars
+    fun addCar(newCar: Car) {
+        _cars.value = _cars.value?.plus(newCar)
+    }
+    fun fetchCars() {
+        carsCollection.get()
+            .addOnSuccessListener { result ->
+                val carList = mutableListOf<Car>()
+                for (document in result) {
+                    val car = document.toObject<Car>()
+                    carList.add(car)
+                }
+                _cars.value = carList
+            }
+            .addOnFailureListener { exception ->
+                Log.w("CarViewModel", "Error getting documents: ", exception)
+            }
+    }
+}
